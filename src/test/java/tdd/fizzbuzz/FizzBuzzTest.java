9757cbbfc64bdf10ac4e01bea9287e4add0b6edd
Alex Liu <liual@oocl.com>
2020-11-26 14:03:30 +0800

    [Fizzbuzz][Alex Liu] test: added test case for normal input

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,15 @@
 package tdd.fizzbuzz;
 
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
 public class FizzBuzzTest {
+    @Test
+    public void should_return_1_when_input_is_1() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String order = fizzBuzz.countOrder(1);
+
+        assertEquals("1", order);
+    }
 }
