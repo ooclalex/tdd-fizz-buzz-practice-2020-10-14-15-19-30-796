1b3e25622e7d444a7e8f82dccdcfbb53c0fca605
Alex Liu <liual@oocl.com>
2020-11-26 14:13:31 +0800

    [Fizzbuzz][Alex Liu] feat: implemented countOff for input is multiple of 5

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -5,6 +5,9 @@ public class FizzBuzz {
         if (i % 3 == 0) {
             return "Fizz";
         }
+        else if (i % 5 == 0) {
+            return "Buzz";
+        }
         return String.valueOf(i);
     }
 }
