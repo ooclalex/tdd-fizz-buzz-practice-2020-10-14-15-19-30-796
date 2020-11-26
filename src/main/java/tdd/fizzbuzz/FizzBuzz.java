781997a218c09b2c3c6922694839101389fae852
Alex Liu <liual@oocl.com>
2020-11-26 14:20:47 +0800

    [Fizzbuzz][Alex Liu] feat: implemented countOff for input is multiple of 21

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,9 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int i) {
+        if (i % 21 == 0) {
+            return "FizzWhizz";
+        }
         if (i % 15 == 0) {
             return "FizzBuzz";
         }
