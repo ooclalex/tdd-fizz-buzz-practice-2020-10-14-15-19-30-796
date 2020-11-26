8d1c426467f894b311f703b83f3f13c4d48400b9
Alex Liu <liual@oocl.com>
2020-11-26 14:11:12 +0800

    [Fizzbuzz][Alex Liu] feat: implemented countOff for input is multiple of 3

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,9 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
     public String countOff(int i) {
+        if (i % 3 == 0) {
+            return "Fizz";
+        }
         return String.valueOf(i);
     }
 }
